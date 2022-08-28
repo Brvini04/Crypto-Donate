73dd130b6ac4300ef24cc035034bc51baf016686
Brvini04 <83612401+Brvini04@users.noreply.github.com>
2022-08-28 17:58:31 -0300

    Create donate.sol

---
+++ donate.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+contract Donate{
+
+    address public carteira;
+        function addCarteira(address addressCarteira) external returns(address){
+            carteira = addressCarteira;
+            return carteira;
+        }
+    
+
+        function donate() public payable{
+            payable(carteira).transfer(msg.value / 2);
+        }
+}
